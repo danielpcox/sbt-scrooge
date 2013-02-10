8ca399a9a54506698e1fd3f68bbd334817dbdefe
Colin Godsey <crgodsey@Colins-Mac-Pro.local>
2013-02-10 11:47:50 -0600

    quick fix for 12.1, sorry twitter

--- project/Build.scala
+++ project/Build.scala
@@ -1,20 +1,21 @@
 import sbt._
 import Keys._
-import com.twitter.sbt._
+//import com.twitter.sbt._
 
 object SbtScroogePlugin extends Build {
   lazy val root = Project(
-    id = "sbt11-scrooge",
+    id = "sbt12-scrooge",
     base = file("."),
-    settings = StandardProject.newSettings ++
+    settings = /*StandardProject.newSettings ++
       SubversionPublisher.newSettings ++
-      ScriptedPlugin.scriptedSettings
+      ScriptedPlugin.scriptedSettings*/
+        Defaults.defaultSettings
   ).settings(
-    name := "sbt11-scrooge",
+    name := "sbt12-scrooge",
     organization := "com.twitter",
-    version := "3.0.1-SNAPSHOT",
-    sbtPlugin := true,
+    version := "3.0.1-local",
+    sbtPlugin := true
 
-    SubversionPublisher.subversionRepository := Some("https://svn.twitter.biz/maven-public")
+    //SubversionPublisher.subversionRepository := Some("https://svn.twitter.biz/maven-public")
   )
 }
